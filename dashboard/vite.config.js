a258740f1f7975a0dce7b51f1f4c36157b553306
JhonDoe <133117449+ChikoDoe@users.noreply.github.com>
2026-03-08 13:16:35 +0700

    Update vite.config.js

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -6,9 +6,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
+      '/ws': { target: 'ws://localhost:3001', ws: true },
     }
   },
   build: {
     outDir: '../backend/dashboard/dist',
+    emptyOutDir: true,
   }
 })
